1b631e514bc813b763edb1eb86269c862520cc89
Travis Nguyen <travisn714@gmail.com>
2018-10-02 13:12:47 -0700

    Create React elements in the component that takes a BootStrap Form. #1

--- src/form.js
+++ src/form.js
@@ -1,5 +1,27 @@
 import React from 'react'
 
 export default function Form(props) {
-
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="exampleQuestion">Question</label>
+        <input
+          name="question"
+          type="question"
+          className="form-control"
+          onChange={props.onChange}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleAnswer">Answer</label>
+        <input
+          name="answer"
+          type="answer"
+          className="form-control"
+          onChange={props.onChange}/>
+      </div>
+      <button
+        type="save"
+        className="btn btn-primary">Save</button>
+    </form>
+  )
 }
