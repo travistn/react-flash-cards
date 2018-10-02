a3f16ae7d92c226cfd39faad89d87b4bbe78965d
Travis Nguyen <travisn714@gmail.com>
2018-10-02 15:38:24 -0700

    Change CSS styling to BootStrap styling. #1

--- src/form.js
+++ src/form.js
@@ -4,10 +4,13 @@ export default function Form(props) {
   return (
     <form
       onSubmit={props.onSubmit}
-      className="card form-container shadow">
+      className="card shadow w-50 p-2 border-info form-container rounded">
       <div className="card-body">
+        <p className="h3 mb-5 font-weight-light text-center">Create a Flash Card</p>
         <div className="form-group">
-          <label htmlFor="exampleQuestion">Question</label>
+          <label
+            htmlFor="exampleQuestion"
+            className="font-weight-bold mb-2">Question</label>
           <input
             name="question"
             type="question"
@@ -16,7 +19,9 @@ export default function Form(props) {
             onChange={props.onChange}/>
        </div>
         <div className="form-group">
-          <label htmlFor="exampleAnswer">Answer</label>
+          <label
+            htmlFor="exampleAnswer"
+            className="font-weight-bold mb-2">Answer</label>
           <input
             name="answer"
             type="answer"
@@ -24,10 +29,10 @@ export default function Form(props) {
             value={props.value}
             onChange={props.onChange}/>
         </div>
-          <div className="btn-div">
+          <div className="mt-5 flex text-center">
         <button
           type="save"
-          className="btn btn-primary">Save</button>
+          className="btn btn-info">Save</button>
           </div>
       </div>
     </form>
