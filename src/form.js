fb5fe91a8c3f24cd1824bb41a40f9e245cd34444
Travis Nguyen <travisn714@gmail.com>
2018-10-02 15:55:32 -0700

    Revise Form component to include handleChange, this.state, and handleSave #1

--- src/form.js
+++ src/form.js
@@ -1,40 +1,73 @@
 import React from 'react'
 
-export default function Form(props) {
-  return (
-    <form
-      onSubmit={props.onSubmit}
-      className="card shadow w-50 p-2 border-info form-container rounded">
-      <div className="card-body">
-        <p className="h3 mb-5 font-weight-light text-center">Create a Flash Card</p>
-        <div className="form-group">
-          <label
-            htmlFor="exampleQuestion"
-            className="font-weight-bold mb-2">Question</label>
-          <input
-            name="question"
-            type="question"
-            className="form-control"
-            value={props.value}
-            onChange={props.onChange}/>
-       </div>
-        <div className="form-group">
-          <label
-            htmlFor="exampleAnswer"
-            className="font-weight-bold mb-2">Answer</label>
-          <input
-            name="answer"
-            type="answer"
-            className="form-control"
-            value={props.value}
-            onChange={props.onChange}/>
-        </div>
-          <div className="mt-5 flex text-center">
-        <button
-          type="save"
-          className="btn btn-info">Save</button>
+const styles = {
+  form: {
+    position: 'absolute',
+    top: '10rem',
+    left: '20rem',
+    width: '30rem'
+  }
+}
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      question: '',
+      answer: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+  }
+  handleChange(event) {
+    const form = event.target
+    this.setState({
+      [form.name]: form.value
+    })
+  }
+  handleSave(event) {
+    event.preventDefault()
+    console.log(this.state)
+    event.target.reset()
+  }
+  render () {
+    const {value} = this.state
+    return (
+      <form
+        onSubmit={this.handleSave}
+        className="card shadow p-2 border-info rounded"
+        style={styles.form}>
+        <div className="card-body">
+          <p className="h3 mb-5 font-weight-light text-center">Create a Flash Card</p>
+          <div className="form-group">
+            <label
+              htmlFor="exampleQuestion"
+              className="font-weight-bold mb-2">Question</label>
+            <input
+              name="question"
+              type="question"
+              className="form-control"
+              value={value}
+              onChange={this.handleChange}/>
+         </div>
+          <div className="form-group">
+            <label
+              htmlFor="exampleAnswer"
+              className="font-weight-bold mb-2">Answer</label>
+            <input
+              name="answer"
+              type="answer"
+              className="form-control"
+              value={value}
+              onChange={this.handleChange}/>
           </div>
-      </div>
-    </form>
-  )
+            <div className="mt-5 flex text-center">
+          <button
+            type="save"
+            className="btn btn-info">Save</button>
+            </div>
+        </div>
+      </form>
+    )
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import Form from './form'
 
 ReactDOM.render(
-  <App />,
+  <Form />,
   document.getElementById('root')
 )
