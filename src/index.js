45bbd003a425ec455c89cf5e765038dfd3ce2c73
Travis Nguyen <travisn714@gmail.com>
2018-10-03 11:10:39 -0700

    Define a NavBar component with tabs that include Cards & New. #2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import NavBar from './nav-bar'
 
 ReactDOM.render(
-  <App />,
+  <NavBar />,
   document.getElementById('root')
 )
