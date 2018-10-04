7764c15e63fb8643515fac4bcbff157800365b72
Travis Nguyen <travisn714@gmail.com>
2018-10-04 10:10:30 -0700

    Revise the Cards class to remove the styles object and replace with BootStrap. #2

--- src/cards.js
+++ src/cards.js
@@ -1,28 +1,18 @@
 import React from 'react'
 
-const styles = {
-  card: {
-    position: 'absolute',
-    top: '10rem',
-    left: '20rem'
-  },
-  cardBody: {
-    border: '2px solid'
-  }
-}
-
 export default class Cards extends React.Component {
   render() {
     if (this.props.card === 0) {
       return (
-        <div
-          className="text-center mt-3"
-          style={styles.card}>
-          <div className="h2">You have no flash cards.</div>
-          <button
-            type="button"
-            className="btn btn-info mt-5"
-            onClick={this.props.newCardView}>Make one</button>
+        <div className="d-flex justify-content-center mt-5">
+          <div
+            className="text-center mt-3">
+            <div className="h2">You have no flash cards.</div>
+            <button
+              type="button"
+              className="btn btn-info mt-5"
+              onClick={this.props.newCardView}>Make one</button>
+          </div>
         </div>
       )
     }
@@ -35,7 +25,7 @@ export default class Cards extends React.Component {
                 return (
                   <div
                     key={index}
-                    className="card m-2" style={styles.cardBody}>
+                    className="card m-2">
                     <div className="card-body">
                       <p className="card-text">{card.question}</p>
                       <p className="card-text">{card.answer}</p>
--- src/form.js
+++ src/form.js
@@ -2,9 +2,6 @@ import React from 'react'
 
 const styles = {
   form: {
-    position: 'absolute',
-    top: '10rem',
-    left: '20rem',
     width: '30rem'
   }
 }
@@ -31,46 +28,48 @@ export default class Form extends React.Component {
     this.props.onSubmit(card)
     event.target.reset()
   }
-  render () {
+  render() {
     const {value} = this.state
     return (
-      <form
-        onSubmit={this.handleSave}
-        className="card shadow p-2 border-info rounded"
-        style={styles.form}>
-        <div className="card-body">
-          <p className="h3 mb-5 font-weight-light text-center">Create a Flash Card</p>
-          <div className="form-group">
-            <label
-              htmlFor="question"
-              className="font-weight-bold mb-2">Question</label>
-            <input
-              id="question"
-              name="question"
-              type="question"
-              className="form-control"
-              value={value}
-              onChange={this.handleChange}/>
-         </div>
-          <div className="form-group">
-            <label
-              htmlFor="answer"
-              className="font-weight-bold mb-2">Answer</label>
-            <input
-              id="answer"
-              name="answer"
-              type="answer"
-              className="form-control"
-              value={value}
-              onChange={this.handleChange}/>
-          </div>
-            <div className="mt-5 flex text-center">
-          <button
-            type="submit"
-            className="btn btn-info">Save</button>
+      <div className="d-flex justify-content-center mt-5">
+        <form
+          onSubmit={this.handleSave}
+          className="card shadow p-2 border-info rounded"
+          style={styles.form}>
+          <div className="card-body">
+            <p className="h3 mb-5 font-weight-light text-center">Create a Flash Card</p>
+            <div className="form-group">
+              <label
+                htmlFor="question"
+                className="font-weight-bold mb-2">Question</label>
+              <input
+                id="question"
+                name="question"
+                type="question"
+                className="form-control"
+                value={value}
+                onChange={this.handleChange}/>
             </div>
-        </div>
-      </form>
+            <div className="form-group">
+              <label
+                htmlFor="answer"
+                className="font-weight-bold mb-2">Answer</label>
+              <input
+                id="answer"
+                name="answer"
+                type="answer"
+                className="form-control"
+                value={value}
+                onChange={this.handleChange}/>
+            </div>
+            <div className="mt-5 flex text-center">
+              <button
+                type="submit"
+                className="btn btn-info">Save</button>
+            </div>
+          </div>
+        </form>
+      </div>
     )
   }
 }
